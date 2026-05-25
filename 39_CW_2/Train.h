24c805cb1dd56b0abc0af3cb393e4f7dc7da646f
paulbardar <Q.C.tulip@gmail.com>
2026-05-25 20:23:46 +0300

    class Train header

---
+++ 39_CW_2/Train.h
@@ -0,0 +1,29 @@
+#pragma once
+#include<iostream>
+#include<string>
+using std::cout;
+using std::string;
+using std::endl;
+class Train
+{
+
+	string number;
+	string departure;
+	string station;
+
+
+public:
+	Train();
+	Train(string number, string departure, string station);
+
+	string getNumber()const;
+	string getDeparture()const;
+	string getStation()const;
+
+	void setNumber(string number);
+	void setDeparture(string departure);
+	void setStation(string station);
+
+	void showInfo()const;
+};
+
